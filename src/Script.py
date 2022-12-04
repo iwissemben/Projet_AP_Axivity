b9299d0cf934f0f5c9a6e683aeffd9b056e55c57
iwissemben <107781875+iwissemben@users.noreply.github.com>
2022-12-04 20:27:00 +0100

    Importation Fichier csv brut
    Code permettant l'importation du fichier .csv d'enregistrement.

--- src/Script.py
+++ src/Script.py
@@ -5,8 +5,15 @@ Created on Sun Dec  4 19:09:24 2022
 @author: iWiss
 """
 import os
+import csv
+import pandas as pd
 
-filepath=path=os.path.normpath("../dat/Raw/")
+#Importation du fichier CSV
+filepath=os.path.normpath("C:/Users/iWiss/Desktop/donnee/Classeur1.csv")
+data=pd.read_csv(filepath, sep=",",names=["Temps_unix","Ax","Ay","Az"])
 
-
-#test commentaire de code wissem
+#Initialisation des variables
+Times=data.iloc[:,0]
+Ax=data.iloc[:,1]
+Ay=data.iloc[:,2]
+Az=data.iloc[:,3]
